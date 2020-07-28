f59104ad56c3da8ef5403171b3ce90efe411b799
Barry.Baker@noaa.gov <Barry.Baker@m71a1.ncep.noaa.gov>
2020-07-28 19:27:48 +0000

    adding C96 resolution

---
+++ emi_C96/link_fengsha.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+
+#f=${PWD}/../fengsha/bsmfv3
+f=../../../fengsha/bsmfv3
+directory=`pwd`
+res='C96'
+
+for i in 01 02 03 04 05 06 07 08 09 10 11 12; do
+    for t in tile1 tile2 tile3 tile4 tile5 tile6; do
+	pushd ${directory}/${i}/${t}
+	ln -sf ${f}/${res}/${i}/${t}/rdrag.dat .
+	ln -sf ${f}/${res}/${i}/${t}/clay.dat .
+	ln -sf ${f}/${res}/${i}/${t}/sand.dat .
+	ln -sf ${f}/${res}/${i}/${t}/ssm.dat .
+	ln -sf ${f}/${res}/${i}/${t}/uthr.dat .
+	popd
+    done
+done
+
